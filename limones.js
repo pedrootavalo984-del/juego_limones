3918d8a4374c95eceda60e0d0c057e40b5110e76
pedrootavalo984-del <pedrootavalo984@gmail.com>
2026-04-12 23:19:44 -0500

    mover ala izquierda

--- limones.js
+++ limones.js
@@ -3,6 +3,8 @@ let ctx = canvas.getContext("2d")
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=20;
 const ANCHO_PERSONAJE=40;
+let personajeX= canvas.width/2;
+
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -15,5 +17,20 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="yellow"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+}
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla()
+}    
+ //mover derecha   
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+     dibujarPersonaje();
+
+}     
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
